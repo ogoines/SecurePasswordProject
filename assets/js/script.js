93acae0ff479f97d76e8a93c384e694956e7ec9d
OGrunner <orshagoines@gmail.com>
2022-07-10 19:33:27 -0400

    chages to functions in script

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,41 @@
+const charLengthMsg = "Please enter a number length (8-128 characters)";
+const charNumMsg = "Invalid, Please enter a number.";
+const charTypeMsg = "Please select the char types and cases to be included password";
+
+
+
+// Get references to the #generate element
+var generateBtn = document.getElementById("generate");
+
 // Assignment code here
+
+function getPasswordLength(msg = charLengthMsg) {
+//Prompts user to enter a valid char and number length 
+ var desiredLength = prompt(msg.toString());
+  if (isNaN(desiredLength)) {
+    desiredLength = getPasswordLength(charNumMsg.toString());
+    }
+    if (desiredLength< 8 || desiredLength > 128) {
+     desiredLength = getPasswordLength(charLengthMsg);
+    }
+    return parseInt(desiredLength);
+    console.log("madeit");
+  } 
+
+console.log(getPasswordLength());
+
+
+
+
+
+function getPasswordChar(){
+}
+
+
 function generatePassword() {
 }
 generatePassword();
 
-// Get references to the #generate element
-var generateBtn = document.getElementById("generate");
 
 // Write password to the #password input
 function writePassword() {
@@ -17,6 +48,9 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", function(){
-writePassword();
+    //writePassword();
+    var testPassword = document.getElementById("password-area");
+testPassword.textContent = "This may work";
+
 });
 
